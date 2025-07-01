43ce2bdf24be35a49d8fae60b82629b90c958cb9
h-banii <121690516+h-banii@users.noreply.github.com>
2025-07-01 08:11:20 -0300

    host/livecd/iso: Set splash to logo and efi splash to wallpaper

--- host/livecd/iso.nix
+++ host/livecd/iso.nix
@@ -3,7 +3,7 @@
   isoWithCompression ? true,
   distroName,
   modulesPath,
-  pkgs,
+  assets,
   ...
 }:
 {
@@ -11,19 +11,12 @@
     (modulesPath + "/installer/cd-dvd/installation-cd-graphical-base.nix")
   ];
 
-  isoImage =
-    let
-      splashImage = pkgs.fetchurl {
-        url = "https://yt3.googleusercontent.com/RIWEh-tuhMXXExCZ3_BKK0u0lw9GAek4SZk_sMxQksk_utddEBuf-4BUDC8ORScsbnuk6VQ4dA=s720-c-k-c0x00ffffff-no-rj";
-        hash = "";
-      };
-    in
-    {
-      appendToMenuLabel = "";
-      isoBaseName = distroName;
-      inherit splashImage;
-      efiSplashImage = splashImage;
-      grubTheme = null;
-      squashfsCompression = lib.mkIf (isoWithCompression == false) "gzip -Xcompression-level 1";
-    };
+  isoImage = {
+    appendToMenuLabel = "";
+    isoBaseName = lib.mkForce distroName;
+    splashImage = assets.logo;
+    efiSplashImage = assets.wallpaper;
+    grubTheme = null;
+    squashfsCompression = lib.mkIf (isoWithCompression == false) "gzip -Xcompression-level 1";
+  };
 }
