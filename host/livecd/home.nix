dae9e8fe173e5048dcc34d754f43666cdde4c2c1
h-banii <121690516+h-banii@users.noreply.github.com>
2025-07-01 07:52:14 -0300

    host/livecd/home: Open firefox (VTS) and obs on startup

--- host/livecd/home.nix
+++ host/livecd/home.nix
@@ -23,6 +23,8 @@
 
       home.packages = with pkgs; [
         foot
+        firefox
+        obs-studio
       ];
 
       wayland.windowManager.hyprland.enable = true;
@@ -33,13 +35,29 @@
         ];
         bind = [
           "$mod, C, killactive"
-          "$mod, T, foot"
+          "$mod, T, exec, foot"
+          "$mod, O, exec, obs-studio"
+          "$mod, V, exec, firefox https://denchisoft.com/"
         ];
         bindm = [
           "$mod, mouse:272, movewindow"
           "$mod, mouse:273, resizewindow"
         ];
+        windowrule = [
+          "fullscreenstate -1 2, class:firefox"
+        ];
+        exec-once = [
+          "firefox https://denchisoft.com/"
+          "obs"
+        ];
+        general = {
+          gaps_in = 8;
+          gaps_out = 8;
+          border_size = 3;
+          col.active_border = "0xFFA500FF";
+        };
         decoration = {
+          rounding = 10;
           screen_shader = "${./shader.frag}";
         };
       };
