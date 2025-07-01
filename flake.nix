d68f585d133d2cb458046942bcabadcbef3417d6
h-banii <121690516+h-banii@users.noreply.github.com>
2025-07-01 07:20:17 -0300

    flake: Update greeter's logo and vendor_name

--- flake.nix
+++ flake.nix
@@ -18,6 +18,7 @@
       inherit (nixpkgs) lib;
       system = "x86_64-linux";
       pkgs = nixpkgs.legacyPackages.${system};
+      distroName = "MikanOS";
     in
     {
       packages.${system} = {
@@ -27,7 +28,7 @@
               builtins.toJSON {
                 wallpaper = self.assets.wallpaper;
                 icon = "${pkgs.nixos-icons}/share/icons/hicolor/scalable/apps/nix-snowflake.svg";
-                loading_icon = "${pkgs.nixos-icons}/share/icons/hicolor/scalable/apps/nix-snowflake.svg";
+                loading_icon = self.assets.logo;
                 font-family = "M PLUS 2";
                 sessions = [
                   {
@@ -35,7 +36,7 @@
                     cmd = "uwsm start hyprland-uwsm.desktop";
                   }
                 ];
-                vendor_name = "NixOS";
+                vendor_name = distroName;
               }
             );
             greeter = lib.getExe inputs.greeter.packages.${pkgs.stdenv.hostPlatform.system}.default;
@@ -54,8 +55,7 @@
           livecdSystem = nixpkgs.lib.nixosSystem {
             inherit system;
             specialArgs = {
-              distroName = "MikanOS";
-              inherit inputs;
+              inherit inputs distroName;
               assets = self.assets;
               greeter = self.packages.${system}.greeter;
               # isoWithCompression = false; # for faster build, but bigger iso
