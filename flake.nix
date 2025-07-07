c888aff28fe20864c2fb9163f5ad801d299bf4e1
h-banii <121690516+h-banii@users.noreply.github.com>
2025-07-06 22:36:56 -0300

    flake: Move lib to separate file

--- flake.nix
+++ flake.nix
@@ -99,37 +99,7 @@
         inherit distroName;
       };
 
-      lib = {
-        mkIsoVm =
-          {
-            writeShellScriptBin,
-            qemu,
-            OVMF,
-            iso,
-            withUefi ? false,
-          }:
-          writeShellScriptBin "qemu-system-x86_64-${distroName}" ''
-            ${qemu}/bin/qemu-system-x86_64 \
-              ${if withUefi then "-bios ${OVMF.fd}/FV/OVMF.fd" else ""} \
-              -cdrom $(echo ${iso}/iso/*.iso) \
-              $QEMU_OPTS \
-              "$@"
-          '';
-        evalModuleWithoutCheck =
-          module:
-          lib.evalModules {
-            modules = [
-              {
-                options._module.args = lib.mkOption {
-                  internal = true;
-                };
-                config = {
-                  _module.check = false;
-                };
-              }
-            ];
-          };
-      };
+      lib = import ./lib { inherit lib distroName; };
 
       checks = forAllSystems (
         system:
---
+++ lib/default.nix
@@ -0,0 +1,32 @@
+{ lib, distroName }:
+{
+  mkIsoVm =
+    {
+      writeShellScriptBin,
+      qemu,
+      OVMF,
+      iso,
+      withUefi ? false,
+    }:
+    writeShellScriptBin "qemu-system-x86_64-${distroName}" ''
+      ${qemu}/bin/qemu-system-x86_64 \
+        ${if withUefi then "-bios ${OVMF.fd}/FV/OVMF.fd" else ""} \
+        -cdrom $(echo ${iso}/iso/*.iso) \
+        $QEMU_OPTS \
+        "$@"
+    '';
+  evalModuleWithoutCheck =
+    module:
+    lib.evalModules {
+      modules = [
+        {
+          options._module.args = lib.mkOption {
+            internal = true;
+          };
+          config = {
+            _module.check = false;
+          };
+        }
+      ];
+    };
+}
