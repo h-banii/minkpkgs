7ab2d208d6ef69225c7ab1ecb38681d79b3907bc
h-banii <121690516+h-banii@users.noreply.github.com>
2025-07-08 18:13:01 -0300

    docs: modules: Add OptionsDoc component

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -2,6 +2,7 @@
 import { h } from "vue";
 import type { Theme } from "vitepress";
 import DefaultTheme from "vitepress/theme";
+import OptionsDoc from "./components/OptionsDoc.vue";
 import "./style.css";
 
 export default {
@@ -11,7 +12,8 @@ export default {
       // https://vitepress.dev/guide/extending-default-theme#layout-slots
     });
   },
-  enhanceApp({ app, router, siteData }) {
+  enhanceApp({ app }) {
+    app.component("OptionsDoc", OptionsDoc);
     // ...
   },
 } satisfies Theme;
