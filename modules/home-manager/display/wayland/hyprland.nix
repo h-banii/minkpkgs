aa70f5ee477b18f50c4e5bb84212ffedc78e401c
h-banii <121690516+h-banii@users.noreply.github.com>
2025-07-08 16:37:03 -0300

    modules/home-manager: hyprland: Add config option

--- modules/home-manager/display/wayland/hyprland.nix
+++ modules/home-manager/display/wayland/hyprland.nix
@@ -1,47 +1,41 @@
 {
+  inputs,
   minkpkgs,
   release,
   ...
 }@moduleArgs:
 with minkpkgs.lib;
-{ config, lib, pkgs, ... }:
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
 let
   inherit (lib) mkEnableOption;
   system = pkgs.stdenv.hostPlatform.system;
   inherit (minkpkgs.legacyPackages.${system}) assets;
   cfg = module.getConfig moduleArgs config;
+  hmModule =
+    let
+      hmPath = inputs.home-manager.outPath;
+      extendedLib = import "${hmPath}/modules/lib/stdlib-extended.nix" lib;
+    in
+    import "${hmPath}/modules/services/window-managers/hyprland.nix" {
+      inherit config pkgs;
+      lib = extendedLib;
+    };
 in
 {
   options = module.setOptions moduleArgs {
     enable = mkEnableOption "Hyprland config";
     hyprpaper.enable = mkEnableOption "Hyprpaper (wallpaper)";
-    # TODO: Extra config
+    config = hmModule.options.wayland.windowManager.hyprland.settings;
   };
 
   config = {
     wayland.windowManager.hyprland.enable = cfg.enable;
-    wayland.windowManager.hyprland.settings = {
-      "$mod" = "SUPER";
-      monitor = [
-        ",preferred,auto,1"
-      ];
-      bind = [
-        "$mod, C, killactive"
-        "$mod, T, exec, foot"
-        "$mod, O, exec, obs-studio"
-        "$mod, V, exec, firefox https://denchisoft.com/"
-      ];
-      bindm = [
-        "$mod, mouse:272, movewindow"
-        "$mod, mouse:273, resizewindow"
-      ];
-      windowrule = [
-        "fullscreenstate -1 2, class:firefox"
-      ];
-      exec-once = [
-        "firefox https://denchisoft.com/"
-        "obs"
-      ];
+    wayland.windowManager.hyprland.settings = lib.recursiveUpdate {
       general = {
         gaps_in = 8;
         gaps_out = 8;
@@ -50,12 +44,12 @@ in
       };
       decoration = {
         rounding = 10;
-        screen_shader = "${assets.shader}";
+        screen_shader = "${assets.shader}"; # TODO: shader.orange.enable option
         active_opacity = 0.95;
         inactive_opacity = 0.85;
         fullscreen_opacity = 1.00;
       };
-    };
+    } cfg.config;
 
     services.hyprpaper = {
       enable = cfg.hyprpaper.enable;
