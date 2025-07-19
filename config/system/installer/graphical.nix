4dea81cfa2c1db53fac59e34f7cc2da8d79540ec
h-banii <121690516+h-banii@users.noreply.github.com>
2025-07-19 03:09:01 -0300

    config/system/installer: Add basic graphical installer

---
+++ config/system/installer/graphical.nix
@@ -0,0 +1,62 @@
+{
+  assets,
+  minkpkgs,
+  pkgs,
+  ...
+}:
+{
+  isoImage.edition = "graphical";
+
+  nix.settings.experimental-features = [
+    "nix-command"
+    "flakes"
+  ];
+
+  imports = [
+    minkpkgs.nixosModules.default
+    (import ../common/iso.nix "graphical-base")
+  ];
+
+  environment = {
+    systemPackages = with pkgs; [
+      libsForQt5.kpmcore
+      calamares-nixos
+      calamares-nixos-extensions
+      glibcLocales
+      foot
+      hyprland
+    ];
+
+    pathsToLink = [
+      "/share/calamares"
+    ];
+  };
+
+  services.greetd =
+    let
+      hyprlandConfig = pkgs.writeText "hyprland-greeter.conf" ''
+        monitor=,preferred,auto,1
+        exec-once = calamares
+        decoration {
+          screen_shader = ${assets.shader}
+        }
+        $mod = SUPER;
+        bind = $mod, Enter, exec, calamares
+        bind = $mod, T, exec, foot
+        bind = $mod, C, killactive
+        bind = $mod, mouse:272, movewindow
+        bind = $mod, mouse:273, resizewindow
+      '';
+    in
+    {
+      enable = true;
+      settings = {
+        default_session = {
+          user = "nixos";
+          command = "Hyprland --config ${hyprlandConfig}";
+        };
+      };
+    };
+
+  linuxMink = import ./mink.nix;
+}
