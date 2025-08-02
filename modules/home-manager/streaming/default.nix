97075ff3f7ce65d30f7c93d969ec7334411fb507
h-banii <121690516+h-banii@users.noreply.github.com>
2025-08-02 01:35:59 -0300

    modules/home-manager: Remove streaming.enable

--- modules/home-manager/streaming/default.nix
+++ modules/home-manager/streaming/default.nix
@@ -7,30 +7,10 @@ with minkpkgs.lib;
   ...
 }:
 let
-  inherit (lib)
-    mkIf
-    mkDefault
-    mkEnableOption
-    ;
-  cfg = module.getConfig moduleArgs config;
+  _cfg = module.getConfig moduleArgs config;
 in
 {
   imports = [
     (module.import moduleArgs "obs-studio.nix")
   ];
-
-  options = module.setOptions moduleArgs {
-    enable = mkEnableOption null // {
-      description = ''
-        Whether to enable default streaming programs:
-        - obs-studio
-      '';
-    };
-  };
-
-  config = mkIf cfg.enable (
-    module.setOptions moduleArgs {
-      obs-studio.enable = mkDefault true;
-    }
-  );
 }
